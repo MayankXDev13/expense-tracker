9cf6c937b4c8e2691e5fe84e17a58155430e9f66
Mayank <vijaymaynk2003@gmail.com>
2025-11-14 17:07:52 +0530

    improve docker file

--- frontend/src/lib/axiosInstance.ts
+++ frontend/src/lib/axiosInstance.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+console.log("BASE_URL", BASE_URL);
+
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
